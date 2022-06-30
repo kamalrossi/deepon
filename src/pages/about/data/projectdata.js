e1c2a6c97fb9c1058df8e3c577dc183650208e05
kamalrossi <rossikamal@gmail.com>
2022-06-30 20:37:00 +0600

    first commit

---
+++ src/pages/about/data/projectdata.js
@@ -0,0 +1,49 @@
+const projectowner = "Rossi Kamal(rossikamal@gmail.com)";
+const projectname = "deepon";
+const projectdesc = {
+  title: "Title",
+  desc:
+    "Deepon is an online EdTech platform with landing page, admin dashboard. Major modules include attendance, tuition, grade, teacher, student, course.'"
+};
+const projecttag = [
+  {
+    tag1: "ReactJS",
+    tag2: "CSS",
+    tag3: "HTML",
+    tag4: "EdTech"
+  }
+];
+const projecttime = [
+  {
+    title: "UX/UI  Design",
+    from: "Jan 22",
+    to: "Apr 22"
+  },
+  {
+    title: "Responsive Web Development",
+    from: "Jan 22",
+    to: "Apr 22"
+  }
+];
+
+const projectrole = [
+  {
+    title: "UX Design",
+    desc: "Conceptualization of EdTech and prototyping with simple modules."
+  },
+  {
+    title: "Responsive E-Commerce App Development",
+    desc:
+      "Develop a responsive landing page for EdTech Platform."
+    
+  }
+];
+
+export {
+  projectowner,
+  projectname,
+  projectdesc,
+  projectrole,
+  projecttime,
+  projecttag
+};
